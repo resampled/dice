bf88248c5065cd82fe714c6e35faf82c8f298d01
resampled <133955578+resampled@users.noreply.github.com>
2024-08-29 13:32:04 +0930

    partial make cmtbox

--- diceblog/blog/views.py
+++ diceblog/blog/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import BlogPost, BlogUser, BlogComment
 from django.contrib.auth.decorators import login_required
-# from .models import -MODELS-
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 def homepage(request):
     context = {
@@ -17,9 +19,16 @@ class PostListView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
     model = BlogPost
+    # cmtbox (login should be enforced at template level)
+    def post(self, request, *args, **kwargs):
+            if user.is_authenticated and 'cmtsubmit' in request.POST and 'content' in request.POST:
+                BlogComment.create(assigned_post=model,author=user,content="content")
+        
 
 @login_required
 def UserProfile(request):
     context = {
     }
     return render(request, 'user_profile.html', context=context)
+
+
