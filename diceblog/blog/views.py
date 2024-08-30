21fe541004a498172243879a0120eccc2cc41ba1
resampled <133955578+resampled@users.noreply.github.com>
2024-08-30 14:58:47 +0930

    finish working cmtbox!!!!

--- diceblog/blog/views.py
+++ diceblog/blog/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from .models import BlogPost, BlogUser, BlogComment
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 user = get_user_model()
 
@@ -24,14 +24,15 @@ class PostDetailView(generic.DetailView):
     def post(self, request, *args, **kwargs):
             if user.is_authenticated and 'cmtsubmit' in request.POST and 'content' in request.POST:
                 # https://blog.extrovert.dev/2021/05/get-current-logged-in-user-in-django.html
-                BlogComment.create(
+                cmt = BlogComment.create(
                     assigned_post=self.get_object(),
                     author=self.request.user,
-                    content="content-todo replace-"
+                    content=request.POST["content"]
                 )
+                cmt.save()
                 # TODO push this to DB
-                # and make a proper redirect to the GET page
-                return HttpResponse("idk")
+                # and make a proper redirect to the GET page                
+                return HttpResponseRedirect("")
 @login_required
 def UserProfile(request):
     context = {
--- diceblog/dice/settings.py
+++ diceblog/dice/settings.py
@@ -132,7 +132,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
