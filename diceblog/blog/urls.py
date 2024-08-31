7823a162c75573e9b9259967b8414294ee8e9681
resampled <133955578+resampled@users.noreply.github.com>
2024-08-31 10:39:05 +0930

    create url for userlist and link from homepage template

--- diceblog/blog/urls.py
+++ diceblog/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('postlist/', views.PostListView.as_view(), name='post-list'),
+    path('userlist/', views.UserListView.as_view(), name='user-list'),
     path('accounts/profile/', views.UserProfile, name='user-profile'),
     path('post/<slug:author>/<slug:pk>', views.PostDetailView.as_view(), name='post-detail'),
 ]
