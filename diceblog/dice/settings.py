43779783e0f2d7bc95e298ca1c9171ed0ac16058
resampled <133955578+resampled@users.noreply.github.com>
2024-09-14 18:08:41 +0930

    more sensible migrations

--- diceblog/dice/settings.py
+++ diceblog/dice/settings.py
@@ -100,7 +100,12 @@ WSGI_APPLICATION = 'dice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# switcheroo when testing
 DATABASES = {
+    "default2": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get('PGDATABASE'),
